7bb3f0e88ab52f9bd5e643b9af4d93289510e4ea
daeun lee <dix.delee@gmail.com>
2025-06-18 17:54:36 +0900

    feat: use image_picker to select photo (Camera / Gallery)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,20 +28,42 @@ class WebViewScreen extends StatefulWidget {
 
 class WebViewScreenState extends State<WebViewScreen> {
   late final WebViewController _controller;
+  final ImagePicker _picker = ImagePicker();
 
   @override
   void initState() {
     super.initState();
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      //
+      ..addJavaScriptChannel(
+        'NativeChannel',
+        onMessageReceived: (JavaScriptMessage message) async {
+          if (message.message == 'pickImageFromGallery') {
+            await pickImage(ImageSource.gallery);
+          } else if (message.message == 'pickImageFromCamera') {
+            await pickImage(ImageSource.camera);
+          }
+        },
+      )
+      //
       ..loadRequest(Uri.parse('https://fc1d-175-193-34-14.ngrok-free.app/'));
+  }
+
+  Future<void> pickImage(ImageSource source) async {
+    final XFile? image = await _picker.pickImage(source: source);
+    if (image != null) {
+      final bytes = await image.readAsBytes();
+      final base64 = base64Encode(bytes);
+      _controller.runJavaScript(
+        "window.updateProfilePhoto('data:image/png;base64,$base64');",
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(title: const Text('Flutter WebView Screen')),
-      // body: WebViewWidget(controller: _controller),
       body: SafeArea(child: WebViewWidget(controller: _controller)),
     );
   }
